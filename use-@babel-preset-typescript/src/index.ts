c0ff492d1f072144a97ca790b0861372ec42f92f
yjdjiayou <674032206@qq.com>
2020-01-21 13:39:02 +0800

    新增测试无法编译语法的代码

--- use-@babel-preset-typescript/src/index.ts
+++ use-@babel-preset-typescript/src/index.ts
@@ -3,46 +3,66 @@ interface Person {
     age: number
 }
 
-let p: Person = {
-    name: 'abc',
-    age: 18
-};
-
-console.log(p);
-
-class Animal {
-
-    name:string = 'abc';
-
-}
-console.log(new Animal().name);
-
-const obj1 = {
-    num:66,
-    name:'xyz',
-};
-const obj2 = {...obj1};
-console.log(obj2);
-
-let aaa:number;
-aaa = '6';
-
-
-let bbb:string;
-bbb = null;
-
-
-function getA() {
-    import('./aaa').then((res)=>{
-        console.log(res);
-    })
-}
-getA();
-
-
-
+//
+// let p: Person = {
+//     name: 'abc',
+//     age: 18
+// };
+//
+// console.log(p);
+//
+// class Animal {
+//
+//     name:string = 'abc';
+//
+// }
+// console.log(new Animal().name);
+//
+// const obj1 = {
+//     num:66,
+//     name:'xyz',
+// };
+// const obj2 = {...obj1};
+// console.log(obj2);
+//
+// let aaa:number;
+// aaa = '6';
+//
+//
+// let bbb:string;
+// bbb = null;
+//
+//
+// function getA() {
+//     import('./aaa').then((res)=>{
+//         console.log(res);
+//     })
+// }
+// getA();
 
 
+
+// 无法编译的语法
+// 无法编译的语法
+// 无法编译的语法
+
+// namespace BBB{
+//     const name = 'abc';
+// }
+
+// const enum Sex {
+//     man,
+//     woman
+// }
+
+
+// let p2 = {age: 18} as Person;
+// console.log(p2.name);
+// let p3 = <Person>{age: 18};
+// console.log(p3.name);
+
+
+// export = p3;
 
 
 
